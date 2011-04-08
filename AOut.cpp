ecbb95258ad38568c99a17c8566a8551912cc838
7shi <7shi@live.jp>
2011-04-09 08:34:52 +0900

    AOutクラスを導入

---
+++ AOut.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include "pdp11.h"
+
+AOut::AOut(const std::string &file)
+{
+    struct stat st;
+    if (stat(file.c_str(), &st) != 0 || !S_ISREG(st.st_mode) || st.st_size < 16)
+        return;
+
+    FILE *f = fopen(file.c_str(), "rb");
+    if (!f) return;
+
+    std::vector<uint8_t> header(16);
+    fread(&header[0], 1, 16, f);
+    fmagic = read16(header,  0);
+    tsize  = read16(header,  2);
+    dsize  = read16(header,  4);
+    bsize  = read16(header,  6);
+    ssize  = read16(header,  8);
+    entry  = read16(header, 10);
+    pad    = read16(header, 12);
+    relflg = read16(header, 14);
+
+    if (st.st_size - 16 >= tsize + dsize)
+    {
+        image.resize(tsize + dsize);
+        fread(&image[0], 1, image.size(), f);
+    }
+
+    fclose(f);
+}
--- pdp11.h
+++ pdp11.h
@@ -1,7 +1,8 @@
 #ifndef _PDP11_H_
 #define _PDP11_H_
 
-#include <stddef.h>
+#include <cstddef>
+#include <string>
 #include <vector>
 
 inline uint16_t read16(const std::vector<uint8_t> &vec, int pos)
@@ -14,5 +15,14 @@ inline void write16(std::vector<uint8_t> *vec, int pos, uint16_t v)
     (*vec)[pos] = static_cast<uint8_t>(v);
     (*vec)[pos + 1] = static_cast<uint8_t>(v >> 8);
 }
+
+class AOut
+{
+public:
+    uint16_t fmagic, tsize, dsize, bsize, ssize, entry, pad, relflg;
+    std::vector<uint8_t> image;
+
+    AOut(const std::string &file);
+};
 
 #endif
