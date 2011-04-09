c48c9fd3c94c670087693da38748dbc9d3dbfd8a
7shi <7shi@live.jp>
2011-04-09 18:47:40 +0900

    main.cppを追加

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "VM.h"
+
+int main(int argc, char *argv[])
+{
+    std::vector<std::string> args;
+    for (int i = 1; i < argc; i++)
+    {
+        std::string arg = argv[i];
+        if (arg == "-r")
+        {
+            i++;
+            if (i < argc) setroot(argv[i]);
+        }
+        else
+            args.push_back(arg);
+    }
+    if (args.empty())
+    {
+        printf("usage: %s [-r V6ROOT] cmd [args ...]\n", argv[0]);
+        return 1;
+    }
+    AOut aout = args[0];
+    VM vm = &aout;
+    vm.run(args);
+}
